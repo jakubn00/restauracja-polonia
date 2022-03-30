fa4895b56512d3fc14e9481dcc878369f94ef7a2
jakubn00 <kubaniemyski00@gmail.com>
2022-03-30 20:48:32 +0200

    updated pictures in gallery

--- src/components/Gallery/Desktop/GalleryPictures.tsx
+++ src/components/Gallery/Desktop/GalleryPictures.tsx
@@ -14,20 +14,26 @@ import picture9 from "../../../images/gallery/picture9.jpg";
 import picture10 from "../../../images/gallery/picture10.jpg";
 import picture11 from "../../../images/gallery/picture11.jpg";
 import picture12 from "../../../images/gallery/picture12.jpg";
+import picture13 from "../../../images/gallery/picture13.jpg";
+import picture14 from "../../../images/gallery/picture14.jpg";
+import picture15 from "../../../images/gallery/picture15.jpg";
 
 const pictures = [
   { original: picture1, alt: "Pyszne placki po węgiersku" },
   { original: picture2, alt: "Smaczne desery" },
   { original: picture3, alt: "Pyszny obiad" },
-  { original: picture4, alt: "Restauracja Polonia- zdjęcie z zewnątrz" },
-  { original: picture5, alt: "Restauracja Polonia- zdjęcie z zewnątrz" },
-  { original: picture6, alt: "Restauracja Polonia- zdjecie sali" },
-  { original: picture7, alt: "Restauracja Polonia- zdjecie sali" },
-  { original: picture8, alt: "Restauracja Polonia- zdjecie sali" },
+  { original: picture4, alt: "Pyszny rosół" },
+  { original: picture5, alt: "Pyszny obiad" },
+  { original: picture6, alt: "Pyszny obiad" },
+  { original: picture7, alt: "Restauracja Polonia- zdjęcie z zewnątrz" },
+  { original: picture8, alt: "Restauracja Polonia- zdjęcie z zewnątrz" },
   { original: picture9, alt: "Restauracja Polonia- zdjecie sali" },
-  { original: picture10, alt: "Wystrój sali na komunię" },
-  { original: picture11, alt: "Wystrój sali na imprezę" },
-  { original: picture12, alt: "Wystrój sali na imprezę" },
+  { original: picture10, alt: "Restauracja Polonia- zdjecie sali" },
+  { original: picture11, alt: "Restauracja Polonia- zdjecie sali" },
+  { original: picture12, alt: "Restauracja Polonia- zdjecie sali" },
+  { original: picture13, alt: "Wystrój sali na imprezę" },
+  { original: picture14, alt: "Wystrój sali na imprezę" },
+  { original: picture15, alt: "Wystrój sali na imprezę" },
 ];
 
 const GalleryPictures = () => {
